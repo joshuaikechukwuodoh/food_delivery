30b52a19d2fd1219ef6634296b2152e51cd79f5a
Odoh Joshua ikechukwu <113645159+joshuaikechukwuodoh@users.noreply.github.com>
2025-04-07 05:24:27 +0100

    Delete food_deleviry_site directory

--- food_deleviry_site/model/MenuItems.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MenuItems = new mongoose.Schema({
-    itemName: String,
-    itemPrice: Number,
-    itemDescription: String
-});
-
-module.exports = mongoose.model("MenuItem", MenuItems);
